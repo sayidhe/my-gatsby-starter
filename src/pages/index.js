781d9f1f8615634bd8ec3ee1ca7de98c3c2827e0
sayid <herq12315@gmail.com>
2021-04-29 14:36:28 +0800

    add root wrapper to Gatsby Browser and SSR

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,33 @@
 import { graphql, Link as GatsbyLink } from "gatsby";
 import React from "react";
 import { Box, Heading, Link } from "theme-ui";
-import { Layout } from "../components/layout";
 
 export default function IndexPage({ data }) {
   return (
     <>
-      <Layout>
-        {data.allMdx.nodes.map(({ id, excerpt, frontmatter, slug }) => (
-          <Box
-            key={id}
-            as="article"
-            sx={{
-              mb: 4,
-              p: 3,
-              boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1)",
-              border: "1px solid #d1d1d1",
-              borderRadius: "15px",
-            }}
-          >
-            <Link as={GatsbyLink} to={`/${slug}`}>
-              <Heading>{frontmatter.title}</Heading>
-              <Box as="p" variant="styles.p">
-                {frontmatter.date}
-              </Box>
-              <Box as="p" variant="styles.p">
-                {excerpt}
-              </Box>
-            </Link>
-          </Box>
-        ))}
-      </Layout>
+      {data.allMdx.nodes.map(({ id, excerpt, frontmatter, slug }) => (
+        <Box
+          key={id}
+          as="article"
+          sx={{
+            mb: 4,
+            p: 3,
+            boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1)",
+            border: "1px solid #d1d1d1",
+            borderRadius: "15px",
+          }}
+        >
+          <Link as={GatsbyLink} to={`/${slug}`}>
+            <Heading>{frontmatter.title}</Heading>
+            <Box as="p" variant="styles.p">
+              {frontmatter.date}
+            </Box>
+            <Box as="p" variant="styles.p">
+              {excerpt}
+            </Box>
+          </Link>
+        </Box>
+      ))}
     </>
   );
 }
