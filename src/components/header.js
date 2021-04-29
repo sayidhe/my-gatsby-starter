c5ee61fc078c8b6f376f71e1a471c2c362634de7
sayid <herq12315@gmail.com>
2021-04-29 14:43:21 +0800

    add theme toggle to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import { Link as GatsbyLink } from "gatsby";
 import React from "react";
-import { Box, Heading, Link } from "theme-ui";
+import { Box, Button, Flex, Heading, Link, useColorMode } from "theme-ui";
 
 export const Header = ({ siteTitle, siteDescription }) => {
+  const [colorMode, setColorMode] = useColorMode();
   return (
     <Box as="header" sx={{ bg: "highlight", mb: "1.45rem" }}>
       <Box
@@ -13,12 +14,23 @@ export const Header = ({ siteTitle, siteDescription }) => {
           p: "1.45rem 1.0875rem",
         }}
       >
-        <Link as={GatsbyLink} to="/">
-          <Heading>{siteTitle}</Heading>
-        </Link>
-        <Box as="p" variant="styles.p">
-          {siteDescription}
-        </Box>
+        <Flex>
+          <Box sx={{ flex: "1 1 auto", flexDirection: "column" }}>
+            <Link as={GatsbyLink} to="/">
+              <Heading>{siteTitle}</Heading>
+            </Link>
+            <Box as="p" variant="styles.p">
+              {siteDescription}
+            </Box>
+          </Box>
+          <Button
+            onClick={(e) => {
+              setColorMode(colorMode === "default" ? "dark" : "default");
+            }}
+          >
+            {colorMode === "default" ? "Dark" : "Light"}
+          </Button>
+        </Flex>
       </Box>
     </Box>
   );
